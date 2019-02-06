c723044278261555519b7530219d8f905ea24f17
ryota.suzuki <getty2017@gmail.com>
2019-02-06 09:41:16 +0900

    状態管理ライブラリの導入

--- build/dev-server.js
+++ build/dev-server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 // `Express`アプリケーションインスタンスを受け取る関数をインポート
 module.exports = app => {
   // HTTPリクエストのbodyの内容をJSONとして解析するようミドルウェアをインストール
-  app.user(bodyParser.json())
+  app.use(bodyParser.json())
 
   // Todo: ここ以降にAPIの実装内容を追加していく
 }
